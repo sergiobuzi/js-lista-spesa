5315e51589d49bd86700167a3f3c54995761c3c4
SergioBuzi <sergio.buzi97@gmail.com>
2023-11-24 17:19:08 +0100

    script: adding comments in js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
 
-//seleziono ul
+//seleziono ul nell'html
 const listaUl = document.getElementById("listaspesa");
 
 //creo la lista della spesa
@@ -10,19 +10,30 @@ const arrayspesa = ["proteine","carboidrati","grassi","frutta"];
 //scorro la lista della spesa
 
 
+//creo un flag e la imposto su zero come punto di partenza
+let i = 0;
 
-i = 0;
-
-//finche la i e minore di arrayspesa allora:
+//finche la flag e minore di arrayspesa allora:
 while (i < arrayspesa.length) {
 
+    //creo una variabile che aumenta aumenta ogni volta che aumenta anche la flag 
     let itemspesa = arrayspesa[i];
+
+    //la flag incrementa
     i++;
+
+    //stampo in console la lista spesa ogni giro di flag
     console.log(itemspesa);
+
+    //creo l'elemento li
     const elementLi = document.createElement("li");
+
+    //dentro questo elemento ci appendo gli oggetti della spesa
     elementLi.append(itemspesa);
+
+    //dopo di che appendo gli elementi li dentro l'ul
     listaUl.append(elementLi); 
-    
+
 }
 
 
